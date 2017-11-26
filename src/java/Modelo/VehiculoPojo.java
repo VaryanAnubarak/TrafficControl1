c26c808810de2861d59d07059db8a07f77e0cfbf
Varyan <varyananubarak@gmail.com>
2017-11-26 00:41:20 -0500

    |Code|Vehiculo pojo añadido

--- src/java/Modelo/VehiculoPojo.java
+++ src/java/Modelo/VehiculoPojo.java
@@ -5,10 +5,56 @@
  */
 package Modelo;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
 /**
  *
  * @author jairo
  */
-public class VehiculoPojo {
+@Entity
+@Table
+@NamedQueries(@NamedQuery(name="VehiculoPojo.getAll",query="SELECT e FROM Vehiculo e"))
+public class VehiculoPojo implements Serializable{
+    @Id
+    private String id;
+    @Column
+    private  String matricula;
+    @Column
+    private  String claseVehiculo;
+
+    public VehiculoPojo(String matricula, String claseVehiculo) {
+        this.matricula = matricula;
+        this.claseVehiculo = claseVehiculo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getClaseVehiculo() {
+        return claseVehiculo;
+    }
+
+    public void setClaseVehiculo(String claseVehiculo) {
+        this.claseVehiculo = claseVehiculo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
     
 }
